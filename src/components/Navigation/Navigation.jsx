ec8eda132106a2322ae4921ea331a1cb2bfe7ede
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-11-17 17:45:28 +0200

    fix start code

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,20 +1,21 @@
-// import { NavLink } from 'react-router-dom';
-// import { useAuth } from 'hooks';
-// import css from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks';
+import css from './Navigation.module.css';
 
 export const Navigation = () => {
-  //   const { isLoggedIn } = useAuth();
+  // const { isLoggedIn } = useAuth();
+  const isLoggedIn = false;
 
   return (
     <nav>
-      {/* <NavLink className={css.link} to="/">
+      <NavLink className={css.link} to="/">
         Home
       </NavLink>
       {isLoggedIn && (
-        <NavLink className={css.link} to="/tasks">
+        <NavLink className={css.link} to="/contacts">
           Tasks
         </NavLink>
-      )} */}
+      )}
     </nav>
   );
 };
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,3 +3,4 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 export const register = createAsyncThunk();
 export const logIn = createAsyncThunk();
 export const logOut = createAsyncThunk();
+export const refreshUser = createAsyncThunk();
