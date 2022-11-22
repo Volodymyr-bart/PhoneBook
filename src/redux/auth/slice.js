f60322376b1f25f2aa23fb50f9f3533ecd215eca
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-11-22 16:21:13 +0200

    install material UI

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -43,39 +43,39 @@ const authSlice = createSlice({
       .addCase(refreshUser.rejected, (state, action) => {
         state.isRefreshing = false;
       }),
-
-  // extraReducers: {
-  //   [register.fulfilled](state, action) {
-  //     state.user = action.payload.user;
-  //     state.token = action.payload.token;
-  //     state.isLoggedIn = true;
-  //   },
-  //   [register.rejected](state, action) {
-  //     state.error = action.payload;
-  //     state.isLoading = false;
-  //   },
-  //   [logIn.fulfilled](state, action) {
-  //     state.user = action.payload.user;
-  //     state.token = action.payload.token;
-  //     state.isLoggedIn = true;
-  //   },
-  //   [logOut.fulfilled](state) {
-  //     state.user = { name: null, email: null };
-  //     state.token = null;
-  //     state.isLoggedIn = false;
-  //   },
-  //   [refreshUser.pending](state) {
-  //     state.isRefreshing = true;
-  //   },
-  //   [refreshUser.fulfilled](state, action) {
-  //     state.user = action.payload;
-  //     state.isLoggedIn = true;
-  //     state.isRefreshing = false;
-  //   },
-  //   [refreshUser.rejected](state) {
-  //     state.isRefreshing = false;
-  //   },
-  // },
 });
 
 export const authReducer = authSlice.reducer;
+
+// extraReducers: {
+//   [register.fulfilled](state, action) {
+//     state.user = action.payload.user;
+//     state.token = action.payload.token;
+//     state.isLoggedIn = true;
+//   },
+//   [register.rejected](state, action) {
+//     state.error = action.payload;
+//     state.isLoading = false;
+//   },
+//   [logIn.fulfilled](state, action) {
+//     state.user = action.payload.user;
+//     state.token = action.payload.token;
+//     state.isLoggedIn = true;
+//   },
+//   [logOut.fulfilled](state) {
+//     state.user = { name: null, email: null };
+//     state.token = null;
+//     state.isLoggedIn = false;
+//   },
+//   [refreshUser.pending](state) {
+//     state.isRefreshing = true;
+//   },
+//   [refreshUser.fulfilled](state, action) {
+//     state.user = action.payload;
+//     state.isLoggedIn = true;
+//     state.isRefreshing = false;
+//   },
+//   [refreshUser.rejected](state) {
+//     state.isRefreshing = false;
+//   },
+// },
