5c941aa6c8b73df9e688f11ef358038e9f59670e
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-11-23 12:41:13 +0200

    fix login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import css from './LoginForm.module.css';
@@ -24,15 +25,33 @@ export const LoginForm = () => {
 
   return (
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
+      {/* <span>Welcome</span> */}
+      <TextField
+        id="outlined-basic"
+        label="Please write your email"
+        variant="outlined"
+        type="email"
+        name="email"
+      />
+      <TextField
+        id="outlined-basic"
+        label="Please write your password"
+        variant="outlined"
+        type="password"
+        name="password"
+      />
+      {/* <label className={css.label}>
         Email
         <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
+      </label> */}
+      {/* <label className={css.label}>
         Password
         <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
+      </label> */}
+      <Button variant="contained" type="submit">
+        Log In
+      </Button>
+      {/* <button type="submit">Log In</button> */}
     </form>
   );
 };
