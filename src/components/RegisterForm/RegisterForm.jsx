45a5bbfb0c722cce075c343df86599148a7b10f6
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-11-23 12:45:57 +0200

    fix register

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 import css from './RegisterForm.module.css';
@@ -20,19 +21,43 @@ export const RegisterForm = () => {
 
   return (
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
+      <TextField
+        id="outlined-basic"
+        label="Username"
+        variant="outlined"
+        type="text"
+        name="name"
+      />
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+        type="email"
+        name="email"
+      />
+      <TextField
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        type="password"
+        name="password"
+      />
+      {/* <label className={css.label}>
         Username
         <input type="text" name="name" />
-      </label>
-      <label className={css.label}>
+      </label> */}
+      {/* <label className={css.label}>
         Email
         <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
+      </label> */}
+      {/* <label className={css.label}>
         Password
         <input type="password" name="password" />
-      </label>
-      <button type="submit">Register</button>
+      </label> */}
+      <Button variant="contained" type="submit">
+        Register
+      </Button>
+      {/* <button type="submit">Register</button> */}
     </form>
   );
 };
