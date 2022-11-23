5445a58991c5bbad9cc5a2499d0f5f4ae81573a8
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-11-23 12:14:57 +0200

    23.11 start

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
 import css from './Navigation.module.css';
-import { Button } from '@mui/material';
+// import { Button } from '@mui/material';
+// import { IconName } from 'react-icons/fc';
+import { FaAddressCard } from 'react-icons/fa';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <nav>
+    <nav className={css.nav}>
       <NavLink className={css.link} to="/">
         Home
       </NavLink>
+      {/* <IoHome /> */}
       {isLoggedIn && (
         <NavLink className={css.link} to="/contacts">
-          Phonebook
+          <FaAddressCard size={'30px'} />
         </NavLink>
       )}
     </nav>
